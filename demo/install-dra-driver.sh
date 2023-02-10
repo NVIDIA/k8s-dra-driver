a6c5d359ad733a19597c662d651562bc89d30cc6
Kevin Klues <kklues@nvidia.com>
2023-02-10 19:23:41 +0000

    Make helm properties per-component between the controller and plugin
    Signed-off-by: Kevin Klues <kklues@nvidia.com>

--- demo/install-dra-driver.sh
+++ demo/install-dra-driver.sh
@@ -1,3 +1,3 @@
-kubectl label node 127.0.0.1 --overwrite nvidia.com/dra.plugin=true
+kubectl label node 127.0.0.1 --overwrite nvidia.com/dra.kubelet-plugin=true
 kubectl label node 127.0.0.1 --overwrite nvidia.com/dra.controller=true
 helm upgrade -i --create-namespace --namespace nvidia-dra-driver nvidia ../deployments/helm/k8s-dra-driver
