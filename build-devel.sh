092906be3e41ec32d63dbc412e1ffff2e8d29904
Kevin Klues <kklues@nvidia.com>
2023-04-05 13:09:47 +0000

    Remove double-saving of image to containerd in build script
    Signed-off-by: Kevin Klues <kklues@nvidia.com>

--- build-devel.sh
+++ build-devel.sh
@@ -6,9 +6,8 @@ REGISTRY=nvcr.io/nvidia/cloud-native
 IMAGE=k8s-dra-driver
 PLATFORM=ubi8
 
+sudo true
 make -f deployments/container/Makefile build-${PLATFORM}
 docker tag ${REGISTRY}/${IMAGE}:${VERSION}-${PLATFORM} ${REGISTRY}/${IMAGE}:${VERSION}
-docker save ${REGISTRY}/${IMAGE}:${VERSION}-${PLATFORM} > image.tgz
-sudo ctr -n k8s.io image import image.tgz
 docker save ${REGISTRY}/${IMAGE}:${VERSION} > image.tgz
 sudo ctr -n k8s.io image import image.tgz
