2e2a33200d0e3707740a885c3b894c186b823978
Evan Lezar <elezar@nvidia.com>
2023-09-13 21:15:26 +0200

    Apply loggingConfig fix
    Apply the loggingConfig fix from
    
    https://github.com/kubernetes-sigs/dra-example-driver/pull/30
    
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

--- cmd/set-nas-status/main.go
+++ cmd/set-nas-status/main.go
@@ -44,9 +44,11 @@ func newApp() *cli.App {
 		status string
 
 		kubeClientConfig flags.KubeClientConfig
-		loggingConfig    flags.LoggingConfig
+		loggingConfig    *flags.LoggingConfig
 		nasConfig        flags.NasConfig
 	)
+
+	loggingConfig = flags.NewLoggingConfig()
 
 	flags := []cli.Flag{
 		&cli.StringFlag{
@@ -67,6 +69,7 @@ func newApp() *cli.App {
 			EnvVars: []string{"STATUS"},
 		},
 	}
+
 	flags = append(flags, kubeClientConfig.Flags()...)
 	flags = append(flags, loggingConfig.Flags()...)
 	flags = append(flags, nasConfig.Flags()...)
