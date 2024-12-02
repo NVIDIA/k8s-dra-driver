d64afb1f31d2b17375691c09abcbb3a0ca420d70
Varun Ramachandra Sekar <vsekar@nvidia.com>
2024-12-02 12:58:42 -0800

    vfio-pci device config API
    Signed-off-by: Varun Ramachandra Sekar <vsekar@nvidia.com>

---
+++ api/nvidia.com/resource/gpu/v1alpha1/driverconfig.go
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1alpha1
+
+import "fmt"
+
+// GpuDriver encodes the gpu driver as a string.
+type GpuDriver string
+
+const (
+	NvidiaDriver  GpuDriver = "nvidia"
+	VfioPciDriver GpuDriver = "vfio-pci"
+)
+
+// GpuDriverConfig holds the set of parameters for configuring a GPU with a driver.
+type GpuDriverConfig struct {
+	Driver GpuDriver `json:"driver"`
+}
+
+// DefaultGpuDriverConfig provides the default configuration of a GPU with a driver.
+func DefaultGpuDriverConfig() *GpuDriverConfig {
+	return &GpuDriverConfig{
+		Driver: NvidiaDriver,
+	}
+}
+
+// Normalize updates a GpuDriverConfig config with implied default values based on other settings.
+func (c *GpuDriverConfig) Normalize() error {
+	if c.Driver == "" {
+		c.Driver = NvidiaDriver
+	}
+	return nil
+}
+
+// Validate ensures that GpuDriverConfig has a valid set of values.
+func (c *GpuDriverConfig) Validate() error {
+	switch c.Driver {
+	case NvidiaDriver:
+		fallthrough
+	case VfioPciDriver:
+		break
+	default:
+		return fmt.Errorf("invalid driver specified in gpu driver configuration")
+	}
+	return nil
+}
--- api/nvidia.com/resource/gpu/v1alpha1/gpuconfig.go
+++ api/nvidia.com/resource/gpu/v1alpha1/gpuconfig.go
@@ -29,7 +29,8 @@ import (
 // GpuConfig holds the set of parameters for configuring a GPU.
 type GpuConfig struct {
 	metav1.TypeMeta `json:",inline"`
-	Sharing         *GpuSharing `json:"sharing,omitempty"`
+	Sharing         *GpuSharing      `json:"sharing,omitempty"`
+	DriverConfig    *GpuDriverConfig `json:"driverConfig,omitempty"`
 }
 
 // DefaultGpuConfig provides the default GPU configuration.
@@ -45,11 +46,26 @@ func DefaultGpuConfig() *GpuConfig {
 				Interval: ptr.To(DefaultTimeSlice),
 			},
 		},
+		DriverConfig: &GpuDriverConfig{
+			Driver: NvidiaDriver,
+		},
 	}
 }
 
 // Normalize updates a GpuConfig config with implied default values based on other settings.
 func (c *GpuConfig) Normalize() error {
+	if c.DriverConfig == nil {
+		c.DriverConfig = DefaultGpuDriverConfig()
+	} else {
+		if err := c.DriverConfig.Normalize(); err != nil {
+			return err
+		}
+	}
+	// If driver is not Nvidia, don't proceed with normalizing sharing configuration.
+	if c.DriverConfig.Driver != NvidiaDriver {
+		return nil
+	}
+
 	if c.Sharing == nil {
 		c.Sharing = &GpuSharing{
 			Strategy: TimeSlicingStrategy,
@@ -68,8 +84,21 @@ func (c *GpuConfig) Normalize() error {
 
 // Validate ensures that GpuConfig has a valid set of values.
 func (c *GpuConfig) Validate() error {
-	if c.Sharing == nil {
-		return fmt.Errorf("no sharing strategy set")
+	if c.DriverConfig.Driver == NvidiaDriver {
+		if c.Sharing == nil {
+			return fmt.Errorf("no sharing strategy set")
+		}
+		if err := c.Sharing.Validate(); err != nil {
+			return err
+		}
+	} else {
+		if c.Sharing != nil {
+			return fmt.Errorf("sharing strategy cannot be provided while using non-nvidia driver")
+		}
 	}
-	return c.Sharing.Validate()
+	if err := c.DriverConfig.Validate(); err != nil {
+		return err
+	}
+
+	return nil
 }
